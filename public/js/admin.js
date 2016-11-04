8b2dbb37c332390802a8658a2d28ad7f3986b69d
Alexei Frolov <alexeifrolov3@gmail.com>
2016-11-04 17:18:47 -0400

    Account creation form

--- public/js/admin.js
+++ public/js/admin.js
@@ -2,12 +2,42 @@
 $('#nav-home').addClass('active');
 
 $('#option-accounts').click(function(evt) {
-    $('#modal-accounts').modal('toggle');
+    $('#modal-accounts').modal('show');
     $.ajax({
         type: 'GET',
         url: '/studentlist',
         success: function(data) {
             $('#account-table').html(data);
+        }
+    });
+});
+
+$('#create-user').click(function(evt) {
+    $('#modal-accounts').modal('hide');
+    $('#modal-createuser').modal('show');
+});
+
+$('#userform').submit(function(evt) {
+    evt.preventDefault();
+
+    var fields = $('#userform').serializeArray();
+    var user = {};
+
+    jQuery.each(fields, function(i, field) {
+        user[field.name] = field.value;
+    });
+
+    $.ajax({
+        type: 'POST',
+        url: '/useradd',
+        data: user,
+        success: function(data) {
+            if (data == 'failure')
+                $('#result').html('User could not be added');
+            else if (data == 'exists')
+                $('#result').html('User ' + user.id + ' already exists');
+            else if (data == 'success')
+                $('#result').html('User ' + user.id + ' added to database');
         }
     });
 });
