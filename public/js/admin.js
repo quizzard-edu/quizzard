907345f457780859cc6dd05b9a1c84280d87bfb1
mehtadarshan <darshanmehta786@yahoo.in>
2017-10-20 00:16:59 -0400

    fixed radio buttons

--- public/js/admin.js
+++ public/js/admin.js
@@ -170,8 +170,8 @@ String.prototype.format = function() {
 var mcAnswerCount = 4;
 var addMCAnswers = function(dom){
     mcAnswerCount++;
-    var newdiv = document.createElement('div');
-    var inputdiv = '<p><input type="radio" name="radbutton" value="mcans{0}" id="mcans{1}" required/><label for="mcans{2}">Is Correct</label><input type="text" name="mcans{3}" placeholder="Enter Answer Here" required style="float:left;" class="form-control"/><a onclick="$(this).parent().remove()" class="btn-floating btn-tiny waves-effect waves-light red"><i class="tiny material-icons">close</i></a></p>';
+    var newdiv = document.createElement('p');
+    var inputdiv = '<input type="radio" name="radbutton" value="mcans{0}" id="mcans{1}" required/><label for="mcans{2}">Is Correct</label><input type="text" name="mcans{3}" placeholder="Enter Answer Here" required style="float:left;" class="form-control"/><a onclick="$(this).parent().remove()" class="btn-floating btn-tiny waves-effect waves-light red"><i class="tiny material-icons">close</i></a>';
     newdiv.innerHTML = inputdiv.format(mcAnswerCount,mcAnswerCount,mcAnswerCount,mcAnswerCount);
     $('#qAnswer > div.form-group').append(newdiv);
 }
