1900fd99474e9e017f0e45a143c717137ffb5980
Ramy Esteero <esteeroramy@gmail.com>
2017-10-19 20:47:25 -0400

    Created basic UI rating

---
+++ public/js/rating.js
@@ -0,0 +1,28 @@
+var rating = -1;
+
+/**
+ Highlights the stars based on the rating `selectedStar` given
+*/
+function highlight(selectedStar) {
+  for (i = 0; i <= parseInt(selectedStar); i++) {
+    $('#' + i).html('star');
+  }
+
+  for (i = parseInt(selectedStar) + 1; i <= 4; i++) {
+    $('#' + i).html('star_border');
+  }
+}
+
+/**
+ Gets the rating selected
+*/
+function getRating() {
+  return rating
+}
+
+/**
+ Sets the rating
+*/
+function setRating(newRating) {
+  rating = newRating;
+}
