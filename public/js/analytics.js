9d310c3ec19c7d550ceeb7f53e9ffecd58896b14
Ramy Esteero <esteeroramy@gmail.com>
2017-10-21 22:02:54 -0400

    Added tabs for the professor

--- public/js/analytics.js
+++ public/js/analytics.js
@@ -1,11 +1,32 @@
 $(function() {
   $('#nav-analytics').addClass('active');
 
+  displayClassStatistics();
   getQuestionsAnsweredVsClass();
   getAccuracyVsClass();
   getPointsVsClass();
   getRatingVsClass();
 });
+
+$('#option-class').click(function(evt) {
+    displayClassStatistics();
+});
+
+$('#option-student').click(function(evt) {
+    displayStudentStatistics();
+});
+
+var displayClassStatistics = function() {
+    // Card visibilty
+    $('#student-analytics-card').addClass('hidden');
+    $('#instructor-analytics-card').removeClass('hidden');
+}
+
+var displayStudentStatistics = function() {
+    // Card visibilty
+    $('#student-analytics-card').removeClass('hidden');
+    $('#instructor-analytics-card').addClass('hidden');
+}
 
 var getQuestionsAnsweredVsClass = function() {
   $.ajax({
