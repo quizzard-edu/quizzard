7d5b2973b8c02ce405cb093c95ff24852a4f443e
Mohammed Khan <mohammedjs.khan@mail.utoronto.ca>
2017-10-25 23:54:54 -0400

    Got the rating to fit int SweetAlert and in an answered question. Really dirty fix.

--- public/js/question.js
+++ public/js/question.js
@@ -19,11 +19,16 @@ var sendAnswerRequest = function(ans) {
         data: { questionId: questionId, answer: ans },
         success: function(data) {
             swal({
+                html:true,
                 title: 'Correct',
-                text: 'Congratulations! You gained ' + data.points + ' points!',
+                text: 'Congratulations! You gained ' + data.points + ' points!<br><br>Please rate the difficulty of this question:<br><div class="rating"><i class="medium material-icons" id="0" onmouseover="highlight(0)" onmouseout="highlight(rating)" onclick="setRating(1)">star_border</i><i class="medium material-icons" id="1" onmouseover="highlight(1)" onmouseout="highlight(rating)" onclick="setRating(2)">star_border</i><i class="medium material-icons" id="2" onmouseover="highlight(2)" onmouseout="highlight(rating)" onclick="setRating(3)">star_border</i><i class="medium material-icons" id="3" onmouseover="highlight(3)" onmouseout="highlight(rating)" onclick="setRating(4)">star_border</i><i class="medium material-icons" id="4" onmouseover="highlight(4)" onmouseout="highlight(rating)" onclick="setRating(5)">star_border</i></div>',
                 type: 'success'
-            }, function () {
-                window.location.href = '/';
+            }, function (isConfirm) {
+                if(isConfirm) {
+                    alert(getRating() + " " + questionId)
+                    submitQuestionRating(getRating(), questionId);
+                    window.location.href = '/';
+                }
             });
         },
         error: function(data){
