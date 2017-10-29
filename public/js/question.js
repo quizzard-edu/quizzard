adebf0362a9db269a0ea7585b6cadb75b5d0f775
Mohammed Khan <mohammedjs.khan@mail.utoronto.ca>
2017-10-28 21:45:19 -0400

    Added a clause where it doesn't run the rating submit if the user doesnt enter a rating

--- public/js/question.js
+++ public/js/question.js
@@ -43,10 +43,11 @@ var sendAnswerRequest = function(ans) {
                 type: 'success'
             }, function (isConfirm) {
                 if(isConfirm) {
-                    alert(getRating() + " " + questionId)
-                    submitQuestionRating(getRating(), questionId)
-                    window.location.href = '/';
+                    if(getRating() > 0) {
+                        submitQuestionRating(getRating(), questionId);
+                    }
                 }
+                window.location.href = '/';
             });
         },
         error: function(data) {
