100d34d20aef98dd9f0c5de3c30ac6e022b7a5ae
Ramy Esteero <esteeroramy@gmail.com>
2017-11-06 22:44:10 -0500

    added the cancel button

--- public/js/profile.js
+++ public/js/profile.js
@@ -4,6 +4,13 @@
 * @method enableEdit
 */
 var enableEdit = function() {
-    $('viewForm').addClass('hidden');
-    $('exitForm').removeClass('hidden');
+    $('#viewForm').addClass('hidden');
+    $('#editForm').removeClass('hidden');
+    $('#cancelButton').removeClass('hidden');
+}
+
+var disableEdit = function() {
+    $('#viewForm').removeClass('hidden');
+    $('#editForm').addClass('hidden');
+    $('#cancelButton').addClass('hidden');
 }
