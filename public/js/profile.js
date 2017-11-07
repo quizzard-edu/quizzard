d93bee60efd4fea3ca5e6075f3c154b57597cc7a
Ramy Esteero <esteeroramy@gmail.com>
2017-11-06 22:18:12 -0500

    more work on profile for the edit button functionality

---
+++ public/js/profile.js
@@ -0,0 +1,9 @@
+/**
+* Changes the view to the editing page
+*
+* @method enableEdit
+*/
+var enableEdit = function() {
+    $('viewForm').addClass('hidden');
+    $('exitForm').removeClass('hidden');
+}
