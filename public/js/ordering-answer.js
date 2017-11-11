7537f48076ad3dc9a0169f90798d3e63c90b5cfe
Darshan Mehta <ca07101174@celestica.com>
2017-11-11 00:28:47 -0500

    fix add questions

--- public/js/ordering-answer.js
+++ public/js/ordering-answer.js
@@ -4,11 +4,11 @@ $(function(){
         revert:true,
         start: function(event,ui) {
             $('#icon1',ui.item).hide();
-            $("#icon2",ui.item).show();
+            $('#icon2',ui.item).show();
         },
         stop: function(event,ui) {
-            $("#icon2",ui.item).hide();
-            $("#icon1",ui.item).show();
+            $('#icon2',ui.item).hide();
+            $('#icon1',ui.item).show();
         }
     });
 });
@@ -17,6 +17,7 @@ $(function(){
 var orderAnswerCount = 4;
 var addOrderingAnswer = function(){
     orderAnswerCount++;
-    var inputdiv = "<div class='row'><li class='input-field col s10'><i class='material-icons prefix'>swap_vert</i><input id='icon_prefix' type='text' placeholder='Enter Answer Here' name='orderItem{0}' required='required' class='validate'/></li><div class='input-field col s2'><a onclick='$(this).parent().parent().remove()' class='btn-floating btn-tiny waves-effect waves-light red'><i class='tiny material-icons'>close</i></a></div></div>";
+    var inputdiv = "<div class='row'><li class='input-field col s10'><i class='material-icons prefix' id='icon1'>swap_vert</i><i class='material-icons prefix' id='icon2'>swap_vertical_circle</i><input class='validate' id='icon_prefix' type='text' name='orderItem{0}' placeholder='Enter Answer Here' required='required' value='1'></li><div class='input-field col s2'><a class='btn-floating btn-tiny waves-effect waves-light red' onclick='$(this).parent().parent().remove()'><i class='tiny material-icons'>close</i></a></div></div>";
     $('#sortable').append(inputdiv.format([orderAnswerCount]));
 }
+
