5a60b6c006d026f135864193ee733ebffe9d6b17
mehtadarshan <darshanmehta786@yahoo.in>
2017-11-11 00:45:27 -0500

    removing random 1  valiue

--- public/js/ordering-answer.js
+++ public/js/ordering-answer.js
@@ -17,7 +17,7 @@ $(function(){
 var orderAnswerCount = 4;
 var addOrderingAnswer = function(){
     orderAnswerCount++;
-    var inputdiv = "<div class='row'><li class='input-field col s10'><i class='material-icons prefix' id='icon1'>swap_vert</i><i class='material-icons prefix' id='icon2'>swap_vertical_circle</i><input class='validate' id='icon_prefix' type='text' name='orderItem{0}' placeholder='Enter Answer Here' required='required' value='1'></li><div class='input-field col s2'><a class='btn-floating btn-tiny waves-effect waves-light red' onclick='$(this).parent().parent().remove()'><i class='tiny material-icons'>close</i></a></div></div>";
+    var inputdiv = "<div class='row'><li class='input-field col s10'><i class='material-icons prefix' id='icon1'>swap_vert</i><i class='material-icons prefix' id='icon2'>swap_vertical_circle</i><input class='validate' id='icon_prefix' type='text' name='orderItem{0}' placeholder='Enter Answer Here' required='required'></li><div class='input-field col s2'><a class='btn-floating btn-tiny waves-effect waves-light red' onclick='$(this).parent().parent().remove()'><i class='tiny material-icons'>close</i></a></div></div>";
     $('#sortable').append(inputdiv.format([orderAnswerCount]));
 }
 
