57276da43e154a6fccf6d7a79e116d278975030b
esteeroramy <esteeroramy@gmail.com>
2017-11-14 09:22:06 -0500

    Added some fields

---
+++ public/js/settings.js
@@ -0,0 +1,34 @@
+$(function() {
+  getQuestionsTopicsList();
+})
+
+
+var getQuestionsTopicsList = function () {
+    $.ajax({
+        async: false,
+        type: 'GET',
+        url: '/questionsListofTopics',
+        success: function(data) {
+            autocompleteTopics = {};
+
+            for (var t in data) {
+                autocompleteTopics[data[t]] = null;
+            }
+
+            // Setting up the autocomplete search for topics
+            $('#qtopic').autocomplete({
+              data: autocompleteTopics,
+              limit: 20,
+              minLength: 0
+            });
+        },
+        error: function(data) {
+            if (data['status'] === 401) {
+                window.location.href = '/';
+            } else {
+                failSnackbar('Sorry, something went wrong, please try again');
+            }
+        }
+    });
+
+}
