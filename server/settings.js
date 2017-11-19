e7a66f12ffa855fceb0133407e8e32342f8092d6
ahmedqarmout2 <ahmedqarmout2@gmail.com>
2017-11-18 23:52:10 -0500

    adding the rest of setters

--- server/settings.js
+++ server/settings.js
@@ -24,8 +24,8 @@ const common = require('./common.js');
 
 /**
  * reset the global settings to their default values
- * 
- * @param {function} callback 
+ *
+ * @param {function} callback
  */
 exports.resetAllSettings = function (callback) {
     db.resetAllSettings (callback);
@@ -33,210 +33,308 @@ exports.resetAllSettings = function (callback) {
 
 /**
  * check if the class is active
- * 
- * @param {funtion} callback 
+ *
+ * @param {funtion} callback
  */
 exports.getClassActive = function (callback) {
     getAllSettings(function (err, allSettings) {
-        if (err) {
-            return callback(err, null);
-        }
-
-        return callback(null, allSettings.general.active);
+        return callback(err ? err : null, err ? null : allSettings.general.active);
     });
 }
 
 /**
  * set class activation status
- * 
+ *
  * @param {boolean} isActive
- * @param {funtion} callback 
+ * @param {funtion} callback
  */
 exports.setClassActive = function (isActive, callback) {
     if (Object.prototype.toString.call(isActive) !== '[object Boolean]') {
         return callback('Invalid input', null);
     }
 
-    var updateQuery = {};
-    updateQuery.$set = {'general.active': isActive};
-    updateSettings(updateQuery, function (err, allSettings) {
-        if (err) {
-            return callback(err, null);
-        }
-
-        return callback(null, 'ok');
-    });
+    updateSettings({$set: {'general.active': isActive}}, callback);
 }
 
 /**
  * get the limit of rows on the leaderboard
- * 
- * @param {funtion} callback 
+ *
+ * @param {funtion} callback
  */
 exports.getLeaderboardLimit = function (callback) {
     getAllSettings(function (err, allSettings) {
-        if (err) {
-            return callback(err, null);
-        }
-
-        return callback(null, allSettings.general.leaderboardLimit);
+        return callback(err ? err : null, err ? null : allSettings.general.leaderboardLimit);
     });
+}
+
+/**
+ * set leaderboard limit
+ *
+ * @param {number} limit
+ * @param {funtion} callback
+ */
+exports.setLeaderboardLimit = function (limit, callback) {
+    if (Object.prototype.toString.call(limit) !== '[object Number]') {
+        return callback('Invalid input', null);
+    }
+
+    updateSettings({$set: {'general.leaderboardLimit': limit}}, callback);
 }
 
 /**
  * check if the student can edit their first and last name
- * 
- * @param {funtion} callback 
+ *
+ * @param {funtion} callback
  */
 exports.getStudentEditNameEnabled = function (callback) {
     getAllSettings(function (err, allSettings) {
-        if (err) {
-            return callback(err, null);
-        }
-
-        return callback(null, allSettings.student.editNames);
+        return callback(err ? err : null, err ? null : allSettings.student.editNames);
     });
+}
+
+/**
+ * set student edit name enabled
+ *
+ * @param {boolean} isActive
+ * @param {funtion} callback
+ */
+exports.setStudentEditNameEnabled = function (isActive, callback) {
+    if (Object.prototype.toString.call(isActive) !== '[object Boolean]') {
+        return callback('Invalid input', null);
+    }
+
+    updateSettings({$set: {'student.editNames': isActive}}, callback);
 }
 
 /**
  * check if the student can edit their email
- * 
- * @param {funtion} callback 
+ *
+ * @param {funtion} callback
  */
 exports.getStudentEditEmailEnabled = function (callback) {
     getAllSettings(function (err, allSettings) {
-        if (err) {
-            return callback(err, null);
-        }
-
-        return callback(null, allSettings.student.editEmail);
+        return callback(err ? err : null, err ? null : allSettings.student.editEmail);
     });
+}
+
+/**
+ * set student edit email enabled
+ *
+ * @param {boolean} isActive
+ * @param {funtion} callback
+ */
+exports.setStudentEditEmailEnabled = function (isActive, callback) {
+    if (Object.prototype.toString.call(isActive) !== '[object Boolean]') {
+        return callback('Invalid input', null);
+    }
+
+    updateSettings({$set: {'student.editEmail': isActive}}, callback);
 }
 
 /**
  * check if the student can edit their password
- * 
- * @param {funtion} callback 
+ *
+ * @param {funtion} callback
  */
 exports.getStudentEditPasswordEnabled = function (callback) {
     getAllSettings(function (err, allSettings) {
-        if (err) {
-            return callback(err, null);
-        }
-
-        return callback(null, allSettings.student.editPassword);
+        return callback(err ? err : null, err ? null : allSettings.student.editPassword);
     });
 }
 
 /**
+ * set student edit password enabled
+ *
+ * @param {boolean} isActive
+ * @param {funtion} callback
+ */
+exports.setStudentEditPasswordEnabled = function (isActive, callback) {
+    if (Object.prototype.toString.call(isActive) !== '[object Boolean]') {
+        return callback('Invalid input', null);
+    }
+
+    updateSettings({$set: {'student.editPassword': isActive}}, callback);
+}
+
+/**
  * get question default topic
- * 
- * @param {funtion} callback 
+ *
+ * @param {funtion} callback
  */
 exports.getQuestionDefaultTopic = function (callback) {
     getAllSettings(function (err, allSettings) {
-        if (err) {
-            return callback(err, null);
-        }
+        return callback(err ? err : null, err ? null : allSettings.question.defaultTopic);
+    });
+}
 
-        return callback(null, allSettings.question.defaultTopic);
+/**
+ * set question default topic
+ *
+ * @param {string} topic
+ * @param {funtion} callback
+ */
+exports.setQuestionDefaultTopic = function (topic, callback) {
+    if (Object.prototype.toString.call(topic) !== '[object String]') {
+        return callback('Invalid input', null);
+    }
+
+    updateSettings({$set: {'question.defaultTopic': topic}}, callback);
+}
+
+/**
+ * get question default min points
+ *
+ * @param {funtion} callback
+ */
+exports.getQuestionDefaultMinPoints = function (callback) {
+    getAllSettings(function (err, allSettings) {
+        return callback(err ? err : null, err ? null : allSettings.question.defaultMinPoints);
     });
 }
 
 /**
  * get question default min points
- * 
- * @param {funtion} callback 
+ *
+ * @param {number} points
+ * @param {funtion} callback
  */
-exports.getQuestionDefaultMinPoints = function (callback) {
-    getAllSettings(function (err, allSettings) {
-        if (err) {
-            return callback(err, null);
-        }
+exports.setQuestionDefaultMinPoints = function (points, callback) {
+    if (Object.prototype.toString.call(points) !== '[object Number]') {
+        return callback('Invalid input', null);
+    }
 
-        return callback(null, allSettings.question.defaultMinPoints);
+    updateSettings({$set: {'question.defaultMinPoints': points}}, callback);
+}
+
+/**
+ * get question default max points
+ *
+ * @param {funtion} callback
+ */
+exports.getQuestionDefaultMaxPoints = function (callback) {
+    getAllSettings(function (err, allSettings) {
+        return callback(err ? err : null, err ? null : allSettings.question.defaultMaxPoints);
     });
 }
 
 /**
  * get question default max points
- * 
- * @param {funtion} callback 
+ *
+ * @param {number} points
+ * @param {funtion} callback
  */
-exports.getQuestionDefaultMaxPoints = function (callback) {
-    getAllSettings(function (err, allSettings) {
-        if (err) {
-            return callback(err, null);
-        }
+exports.setQuestionDefaultMinPoints = function (points, callback) {
+    if (Object.prototype.toString.call(points) !== '[object Number]') {
+        return callback('Invalid input', null);
+    }
 
-        return callback(null, allSettings.question.defaultMaxPoints);
-    });
+    updateSettings({$set: {'question.defaultMaxPoints': points}}, callback);
 }
 
 /**
  * get question timeout enabled
- * 
- * @param {funtion} callback 
+ *
+ * @param {funtion} callback
  */
 exports.getQuestionTimeoutEnabled = function (callback) {
     getAllSettings(function (err, allSettings) {
-        if (err) {
-            return callback(err, null);
-        }
-
-        return callback(null, allSettings.question.timeoutEnabled);
+        return callback(err ? err : null, err ? null : allSettings.question.timeoutEnabled);
     });
+}
+
+/**
+ * set question timeout enabled
+ *
+ * @param {boolean} isActive
+ * @param {funtion} callback
+ */
+exports.setQuestionTimeoutEnabled = function (isActive, callback) {
+    if (Object.prototype.toString.call(isActive) !== '[object Boolean]') {
+        return callback('Invalid input', null);
+    }
+
+    updateSettings({$set: {'question.timeoutEnabled': isActive}}, callback);
 }
 
 /**
  * get question timeout period
- * 
- * @param {funtion} callback 
+ *
+ * @param {funtion} callback
  */
 exports.getQuestionTimeoutPeriod = function (callback) {
     getAllSettings(function (err, allSettings) {
-        if (err) {
-            return callback(err, null);
-        }
-
-        return callback(null, allSettings.question.timeoutPeriod);
+        return callback(err ? err : null, err ? null : allSettings.question.timeoutPeriod);
     });
+}
+
+/**
+ * set question timeout period
+ *
+ * @param {number} timeout
+ * @param {funtion} callback
+ */
+exports.setQuestionTimeoutPeriod = function (timeout, callback) {
+    if (Object.prototype.toString.call(timeout) !== '[object Number]') {
+        return callback('Invalid input', null);
+    }
+
+    updateSettings({$set: {'question.timeoutPeriod': timeout}}, callback);
 }
 
 /**
  * get discussionboard visibility enabled
- * 
- * @param {funtion} callback 
+ *
+ * @param {funtion} callback
  */
 exports.getDiscussionBoardVisibilityEnabled = function (callback) {
     getAllSettings(function (err, allSettings) {
-        if (err) {
-            return callback(err, null);
-        }
-
-        return callback(null, allSettings.discussionBoard.visibility);
+        return callback(err ? err : null, err ? null : allSettings.discussionBoard.visibility);
     });
+}
+
+/**
+ * set discussionboard visibility enabled
+ *
+ * @param {boolean} isActive
+ * @param {funtion} callback
+ */
+exports.setDiscussionBoardVisibilityEnabled = function (isActive, callback) {
+    if (Object.prototype.toString.call(isActive) !== '[object Boolean]') {
+        return callback('Invalid input', null);
+    }
+
+    updateSettings({$set: {'discussionBoard.visibility': isActive}}, callback);
 }
 
 /**
  * get discussionboard dislikes enabled
- * 
- * @param {funtion} callback 
+ *
+ * @param {funtion} callback
  */
 exports.getDiscussionBoardDislikesEnabled = function (callback) {
     getAllSettings(function (err, allSettings) {
-        if (err) {
-            return callback(err, null);
-        }
-
-        return callback(null, allSettings.discussionBoard.dislikesEnabled);
+        return callback(err ? err : null, err ? null : allSettings.discussionBoard.dislikesEnabled);
     });
 }
 
 /**
+ * set discussionboard dislikes enabled
+ *
+ * @param {boolean} isActive
+ * @param {funtion} callback
+ */
+exports.setDiscussionBoardDislikesEnabled = function (isActive, callback) {
+    if (Object.prototype.toString.call(isActive) !== '[object Boolean]') {
+        return callback('Invalid input', null);
+    }
+
+    updateSettings({$set: {'discussionBoard.dislikesEnabled': isActive}}, callback);
+}
+
+/**
  * get all settings object
- * 
- * @param {function} callback 
+ *
+ * @param {function} callback
  */
 exports.getAllSettings = function (callback) {
     getAllSettings(callback);
@@ -244,24 +342,20 @@ exports.getAllSettings = function (callback) {
 
 /**
  * get all settings object
- * 
- * @param {function} callback 
+ *
+ * @param {function} callback
  */
 var getAllSettings = function (callback) {
     db.getAllSettings(function (err, allSettings) {
-        if (err) {
-            return callback(err, null);
-        }
-
-        return callback(null, allSettings);
+        return callback(err ? err : null, err ? null : allSettings);
     });
 }
 
 /**
  * update settings object
- * 
- * @param {object} updateQuery 
- * @param {function} callback 
+ *
+ * @param {object} updateQuery
+ * @param {function} callback
  */
 var updateSettings = function (updateQuery, callback) {
     db.updateSettings({}, updateQuery, callback);
