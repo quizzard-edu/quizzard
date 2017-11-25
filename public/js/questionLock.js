aa125a03ae2f918bad7ddfddda83c449679f099b
mehtadarshan <darshanmehta786@yahoo.in>
2017-11-24 19:25:35 -0500

    pr requests

--- public/js/questionLock.js
+++ public/js/questionLock.js
@@ -9,16 +9,12 @@ $(function () {
 
 function getTimeRemaining(endtime) {
     var t = Date.parse(endtime) - Date.parse(new Date());
-    var seconds = Math.floor((t / 1000) % 60);
-    var minutes = Math.floor((t / 1000 / 60) % 60);
-    var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
-    var days = Math.floor(t / (1000 * 60 * 60 * 24));
     return {
         'total': t,
-        'days': days,
-        'hours': hours,
-        'minutes': minutes,
-        'seconds': seconds
+        'days': Math.floor(t / (1000 * 60 * 60 * 24)),
+        'hours': Math.floor((t / (1000 * 60 * 60)) % 24),
+        'minutes': Math.floor((t / 1000 / 60) % 60),
+        'seconds': Math.floor((t / 1000) % 60)
     };
 }
 
